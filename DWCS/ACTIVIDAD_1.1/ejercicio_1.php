a8432ee1cbd348e3535d7cdfa83d6aa7bd12ece7
KarimBlackStar <gavkops@gmail.com>
2026-09-22 13:48:29 +0200

    a

--- DWCS/ACTIVIDAD_1.1/ejercicio_1.php
+++ DWCS/ACTIVIDAD_1.1/ejercicio_1.php
@@ -9,11 +9,11 @@
     <?php
     
     function calcularIva(float $precioProducto, int $iva = 21) : float{
-        $importeIva = $precioProducto * ($iva / 100);
+        $importeIva = $precioProducto * $iva / 100;
         return $precioProducto + $importeIva;
-    }
-
-    echo "Precio base con 25% IVA: " . calcularIva(2.55, 25) . "€ <br>";
+        }
+        
+        echo "Precio base con 25% IVA: " . calcularIva(2.55, 25) . "€ <br>";
     echo "Precio base 15,45€ con IVA por defecto(21%)" . calcularIva(15.45) . "€";
     ?>
 </body>
